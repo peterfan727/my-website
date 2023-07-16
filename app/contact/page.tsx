17ed58299c490fde70454479f28a0a30589ab548
Peter Fan <70975284+peterfan727@users.noreply.github.com>
2023-07-15 23:48:48 -0700

    temporary contact me page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/800px-LinkedIn_logo_initials.png';
+
 export default function ContactPage() {
     return (
-      <section>
-        <h1>Contact Me</h1>
-        <p>
-          This is my contact page.
-        </p>
-      </section>
+        <>
+            <h1>Contact Me</h1>
+            <div
+                className='w-full p-3 m-3 bg-sky-100 flex justify-center items-center'>
+                <Link 
+                    className='flex flex-col items-center'  
+                    href="https://ca.linkedin.com/in/cc-peter-fan">
+                    <p>Please reach me through LinkedIn!</p>
+                    <Image 
+                        src={logo} 
+                        alt='LinkedIn Logo'
+                        width={100}
+                    />
+                </Link>
+            </div>
+        </>
     );
-  }
+}
