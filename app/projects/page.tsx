dd1a3e6f3183c1fcdaf2627470d448022dad797c
Peter Fan <70975284+peterfan727@users.noreply.github.com>
2023-08-03 16:35:29 -0700

    Added Project cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,52 @@
+import Card from "../components/card";
+import Link from "next/link";
+import Image from "next/image";
+import { all_projects } from "./projects";
+
 export default function ProjectPage() {
+
+    const projects = all_projects.map((p) => {
+        return (
+        <Card>
+                <h2 className='w-full bg-sky-400 p-1'>
+                    {p.name}
+                </h2>
+                <Link href={p.href}>
+                    <Image 
+                    className="hover:shadow-xl mt-3"
+                    src={p.imageHref} width={500} height={300} 
+                    alt={p.imageAlt}/>
+                    <div className="
+                    my-3 p-6 rounded drop-shadow-2xl
+                    text-white bg-blue-600 hover:bg-blue-800 
+                    ">
+                    {p.buttonDescription} 
+                    </div>
+                </Link>
+                <p>{p.description}</p>
+                <p><strong>Tags:<br/></strong>
+                {p.tags.map((t, idx, t_arr)=> {
+                    if (idx == t_arr.length - 1)
+                        return (<span>{t}</span>)
+                    else
+                        return (<span>{t}, </span>)
+                })}</p>
+                <p><strong>Tech:<br/></strong>
+                {p.techs.map((t, idx, t_arr)=> {
+                    if (idx == t_arr.length - 1)
+                        return (<span>{t}</span>)
+                    else
+                        return (<span>{t}, </span>)
+                })}
+                </p>
+        </Card>)
+    })
+
+
     return (
         <>
             <h1>Projects</h1>
-            <p>
-                Sorry, this page is still under construction.
-            </p>
+            {projects}
         </>
     );
 }
