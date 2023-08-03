b6eacf9e1c89d0e0a59e13fe72329c55a1a4d1b4
Peter Fan <70975284+peterfan727@users.noreply.github.com>
2023-08-02 22:47:36 -0700

    Added a link to chatbot on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
 import Country from "./components/country"
 import Map from "./components/map"
 import { headers } from "next/headers"
+import Link from "next/link";
 
 export default function HomePage() {
-  const headersList = headers()
-  const code = headersList.get('X-Geo-Country')
-  const country = Country(code || undefined)
-  return(
-    <>
-        <h1>👋 Hello! Bonjour! Hola! 你好!</h1>
-        <p>
-          {country ?  <>Visitor from {country}! <br/></> : null}
-          Welcome to my humble software developer portfolio
-          <br/>Feel free to pin your city on the guestbook map!
-        </p>
-        <Map  country={country}/>
-    </>
-  )
+    const headersList = headers()
+    const code = headersList.get('X-Geo-Country')
+    const country = Country(code || undefined)
+    return(
+        <>
+            <h1>👋 Hello! Bonjour! Hola! 你好!</h1>
+            <p>
+            {country ?  <>Visitor from {country}! <br/></> : null}
+            Welcome to my humble software developer portfolio
+            <br/>Feel free to pin your city on the guestbook map!
+            </p>
+            <Link href="/chatbot">
+                <div className="
+                    my-3 p-6 rounded drop-shadow-2xl
+                    text-white bg-blue-600 hover:bg-blue-800 
+                ">
+                    🤖 Talk to my GPT chatbot! 
+                </div>
+            </Link>
+            <Map  country={country}/>
+        </>
+    )
 }
