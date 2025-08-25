f074901a1de2c36c6829661588991456cc02cf42
Peter Fan <70975284+peterfan727@users.noreply.github.com>
2025-08-25 03:57:45 +0000

    # This is a combination of 3 commits. # This is the 1st commit message:
    Addressed build errors.
    
    # This is the commit message #2:
    
    Added .nextignore
    
    # This is the commit message #3:
    
    Ignore TS files in embedding

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /app\/projects\/chatbot_v2\/embedding/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
+}
 
 module.exports = nextConfig
